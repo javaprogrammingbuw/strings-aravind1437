7069ccf6a5749f2f883745824a3e2eb12ad18979
aravind1437 <44303218+aravind1437@users.noreply.github.com>
2019-03-25 12:56:57 +0530

    Please check with the assignment

--- Expand.java
+++ Expand.java
@@ -5,8 +5,13 @@ import java.util.Scanner;
 public class Expand {
 	
 	public static void main(String[] args) {
+		
 		Scanner scan = new Scanner(System.in);
 		
+		System.out.print("Enter the your string : ");
+		
+	    String a = scan.nextLine(); 
+
 		//todo: take as an input a combination of characters and numbers (e.g. 2a3b5c)
 		//expand the String by printing each letter so many times as the number before the letter indicates
 		//e.g. 2a3b5c -> aabbbccccc
@@ -14,7 +19,33 @@ public class Expand {
 		//to improve your code such that you can use values greater or equal 10, eg. 10a13b22c
 		//Further modification: if a character should only be printed one time you don't need to write any number infront of that character
 		//e.g. a3b12cd5e -> abbbccccccccccccdeeeee  
-		
+	    
+	    StringBuilder S = new StringBuilder();
+	   
+	    
+	    int dis = 0;
+	    
+	    for (char c : a.toCharArray()) {
+	    	
+	        if (Character.isDigit(c)) {
+	        	
+	            dis = dis * 10 + Character.getNumericValue(c);
+	        } 
+	        else {
+	            while (dis > 0) {
+	            	
+	                S.append(c);
+	                
+	                dis--;
+	            }
+	            
+	            S.append(c);
+	            
+	        }
+	    }
+	    System.out.println( S.toString());
+	}
+	  
 	}
 	
-}
+
--- Palindrome.java
+++ Palindrome.java
@@ -1,8 +1,61 @@
+import java.util.Scanner;
+
 public class Palindrome{
 
 	//todo: Create a method isPalindrome which takes a String as input and returns true if it is a palindrome and false otherwise.
 	//A palindrome is a word which can be read backwards and forwards in the same way., e.g. 'hannah'.
 	//Ignore upper and lower case. 'Hannah' is a palindrome, too.
 	//Ignore also spaces. 'Taco cat' is a valid palindrome.
-
-}
+    static int i;
+	public static void main(String [] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter the string");
+		
+		String a = scan.nextLine();
+		
+		
+		System.out.println("The entered string is = " + a + ", is it a palindrome? " + palin(a) );
+	
+		System.out.println(pal(a));
+		
+	}
+		
+	public static boolean palin(String a) {
+		
+		String c= " ";
+		
+		for(int i = a.length()-1; i>= 0; i--) {
+			
+		c = c + a.charAt(i);
+			
+		}
+		if(c.equals(a)) {
+			
+			return true;		
+		
+		}else {
+		
+			return false;
+		}
+			
+		}
+		
+	
+		public static String pal(String a) {
+		
+	     a = a.toUpperCase(); // changing input to upper case
+	      
+	     a= a.replace(" ", ""); // Removing spaces
+		
+	     return a;
+	      
+	}
+	}
+	
+	
+	
+	
+	
+	
